f8f3d118221f4d462c04bc3328225fcdd59e16f6
Anthony Nishida Mesquita Junior <118089159+satoanthony@users.noreply.github.com>
2024-02-07 20:14:09 -0300

    New styles

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById(".sticky").style.fontSize = "30px";
-  } else {
-    document.getElementById(".sticky").style.fontSize = "90px";
-  }
-}
