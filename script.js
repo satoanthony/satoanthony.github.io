9117bfd4cbf1d2034174ceb6a11a0fdd67d441bc
Anthony Nishida Mesquita Junior <118089159+satoanthony@users.noreply.github.com>
2024-02-06 23:02:27 -0300

    new files

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("header").style.fontSize = "30px";
+    document.getElementById(".sticky").style.fontSize = "30px";
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById(".sticky").style.fontSize = "90px";
   }
 }
